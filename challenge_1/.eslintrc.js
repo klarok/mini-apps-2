d48dca692527553e1dd64c81ba7e3c0a8405e2c7
klarok <karaylu@gmail.com>
2019-01-30 08:57:06 -0800

    Implements dev environment
      - Installs json-server
      - Configures webpack
      - Serves static files (client and data)

---
+++ challenge_1/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = { 
+  "extends": [
+    "airbnb-base",
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "parser": "babel-eslint",
+  "env": {
+    "browser": true,
+    "node": true
+  }
+};
---
+++ challenge_1/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/app.jsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/react'],
+        },
+      },
+    ],
+  },
+  watch: true,
+};
